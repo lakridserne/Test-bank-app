2c96ff965b60a305365dfd7573f9201c5923d2e5
Gordon <gordonmikkelsen87@gmail.com>
2020-03-13 17:09:13 +0100

    dwww

---
+++ IBank.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment1_MockingAndTDD
+{
+  public interface IBank
+    {
+        Account getAccount(String number);
+        void registerAccount(Account account);
+        ICustomer getCustomer(String number);
+        void registerCustomer(ICustomer customer);
+        String getName();
+    }
+}
---
+++ Movement.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment1_MockingAndTDD
+{
+   public class Movement
+    {
+        public Movement(Account source, Account target, long amount, long timestamp) { }
+    }
+}
