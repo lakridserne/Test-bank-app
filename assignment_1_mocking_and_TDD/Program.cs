67a521948cdf93cfbb2c5b67c07476abf4fe2f2b
Gordon <gordonmikkelsen87@gmail.com>
2020-03-13 18:10:28 +0100

    ffdfdf

---
+++ assignment_1_mocking_and_TDD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace assignment_1_mocking_and_TDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
