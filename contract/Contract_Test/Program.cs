dc30328627348c7ce627d65779b7057d56d5495a
Gordon <gordonmikkelsen87@gmail.com>
2020-03-23 19:53:05 +0100

    Createt contract project

---
+++ contract/Contract_Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Contract_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
