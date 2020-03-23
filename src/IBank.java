b705c634d1b37210db016d16f2459a892c6f9c61
Gordon <gordonmikkelsen87@gmail.com>
2020-03-23 14:31:52 +0100

    Added C# project

--- src/IBank.java
+++
@@ -1,8 +0,0 @@
-public interface IBank {
-
-    Account getAccount(String number);
-    void registerAccount(Account account);
-    Customer getCustomer(String number);
-    void registerCustomer(Customer ICustomer);
-    String getName();
-}
