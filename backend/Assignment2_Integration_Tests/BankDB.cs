a1d998d55e1efa90add54530dedb41898d1cba9a
Gordon <gordonmikkelsen87@gmail.com>
2020-04-07 11:56:30 +0200

    created unittest for table

---
+++ backend/Assignment2_Integration_Tests/BankDB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment2_Integration_Tests
+{
+  public class BankDB
+    {
+        public void createTables()
+        {
+
+        }
+
+    }
+}
---
+++ backend/Assignment2_Integration_TestsTests/BankDBTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment2_Integration_Tests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace Assignment2_Integration_Tests.Tests
+{
+    [TestClass()]
+    public class BankDBTests
+    {
+        [TestMethod()]
+        public void createTablesTest()
+        {
+
+            string cs = "Server=127.0.0.1;Database=BankDb;Uid=bank;Pwd=abc123";
+
+            MySqlConnection conn = null;
+            try
+            {
+                conn = new MySqlConnection(cs);
+               conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+
+                cmd.CommandText = "select 1 from information_schema.tables where table_name = 'banktable";
+                cmd.ExecuteNonQuery();
+              
+
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error: {0}", ex.ToString());
+
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
